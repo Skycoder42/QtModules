f29f7022a905f31b3d3b354dc6a45057c17d9b90
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-03-28 23:39:52 +0200

    fixed docker build

--- dockerbuild/qt-build.sh
+++ dockerbuild/qt-build.sh
@@ -11,17 +11,6 @@ mkdir build
 cd build
 git clone --recurse-submodules "$BUILD_GIT_SRC" .
 
-for ex in src tools; do
-	if [ -e $ex ]; then
-		cd $ex
-		for m in $(ls -d */); do
-			mod=${m%%/}
-			echo "CONFIG -= c++1z" >> ./$mod/$mod.pro
-		done
-		cd ..
-	fi
-done
-
 qmake
 make qmake_all
 make
