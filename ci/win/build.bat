76ea7e2e032fff6f3782c6a759e48cd7cd4c7cf3
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-02 17:28:16 +0200

    .

--- ci/win/build.bat
+++ ci/win/build.bat
@@ -23,6 +23,6 @@ if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" (
 	if errorlevel 0 (
 		call %~dp0\build-msvc-first.bat amd64 msvc2015_64 || exit /B 1
 		call %~dp0\build-msvc-first.bat amd64_x86 msvc2015 || exit /B 1
-		call %~dp0\build-mingw53_32.bat || exit /B 1
+		call %~dp0\build-mingw.bat || exit /B 1
 	)
 )
