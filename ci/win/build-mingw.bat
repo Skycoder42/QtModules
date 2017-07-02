b0c1aeb0e4e0c93efeb3695761b003be367ab04b
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-02 17:27:42 +0200

    cleanup

--- ci/win/build-mingw.bat
+++ ci/win/build-mingw.bat
@@ -1,6 +1,7 @@
 setlocal
 
 set qtplatform=mingw53_32
+for %%* in (.) do set CurrDirName=%%~nx*
 
 set PATH=C:\Qt\Tools\mingw530_32\bin;%PATH%;
 set MAKEFLAGS=-j%NUMBER_OF_PROCESSORS%
@@ -10,6 +11,4 @@ cd build-%qtplatform%
 
 C:\Qt\%QT_VER%\%qtplatform%\bin\qmake -r ..\%PROJECT%.pro || exit /B 1
 mingw32-make || exit /B 1
-
-for %%* in (.) do set CurrDirName=%%~nx*
 mingw32-make INSTALL_ROOT=\projects\%CurrDirName%\install install || exit /B 1
--- ci/win/build-msvc-first.bat
+++ ci/win/build-msvc-first.bat
@@ -1,20 +1,17 @@
 :: %1 varsall type
 :: %2 platform
 setlocal
-@echo on
 
 set varsall=%1
 set qtplatform=%2
+for %%* in (.) do set CurrDirName=%%~nx*
 
 call %VC_DIR% %varsall% || exit /B 1
 
-echo BUILDDDDD build-%qtplatform%
 mkdir build-%qtplatform%
 cd build-%qtplatform%
 
 echo about to qmake C:\Qt\%QT_VER%\%qtplatform%\bin\qmake
 C:\Qt\%QT_VER%\%qtplatform%\bin\qmake -r ..\%PROJECT%.pro || exit /B 1
 nmake || exit /B 1
-
-for %%* in (.) do set CurrDirName=%%~nx*
 nmake INSTALL_ROOT=\projects\%CurrDirName%\install install || exit /B 1
