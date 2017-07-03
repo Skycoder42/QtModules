a168baee54e4a0dfe84fb6aba1796fb85d585a8d
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-03 16:18:53 +0200

    fix doc

--- ci/linux/build.sh
+++ ci/linux/build.sh
@@ -8,21 +8,13 @@ export ANDROID_SDK_ROOT=$ANDROID_HOME
 export ANDROID_NDK_ROOT=$ANDROID_NDK
 
 scriptdir=$(dirname $0)
-needs_doc=0
-
 if [[ $EXCLUDE_PLATFORMS != *"linux"* ]]; then
 	$scriptdir/build-all.sh gcc_64 "QMAKE_CXX=g++-5"
-	if (( needs_doc == 0 )); then
-		$scriptdir/build-doc.sh gcc_64
-		needs_doc=1
-	fi
 fi
 
 if [[ $EXCLUDE_PLATFORMS != *"android"* ]]; then
 	$scriptdir/build-first.sh android_armv7
 	$scriptdir/build-first.sh android_x86
-	if (( needs_doc == 0 )); then
-		$scriptdir/build-doc.sh android_armv7
-		needs_doc=1
-	fi
 fi
+
+$scriptdir/build-doc.sh
--- ci/linux/qt-installer-script-base.qs
+++ ci/linux/qt-installer-script-base.qs
@@ -32,8 +32,7 @@ Controller.prototype.TargetDirectoryPageCallback = function() {
 Controller.prototype.ComponentSelectionPageCallback = function() {
     var widget = gui.currentPageWidget();
 	widget.deselectAll();
-	if(pfLinux)
-		widget.selectComponent("qt." + qtVersion + ".gcc_64");
+	widget.selectComponent("qt." + qtVersion + ".gcc_64"); //always install gcc, for doc at least
 	if(pfAndroid) {
 		widget.selectComponent("qt." + qtVersion + ".android_armv7");
 		widget.selectComponent("qt." + qtVersion + ".android_x86");
