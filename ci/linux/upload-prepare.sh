904530d0c3e63d81925c516ec75131d52fbe985e
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-03 13:03:34 +0200

    upload seperate packages

--- ci/linux/upload-prepare.sh
+++ ci/linux/upload-prepare.sh
@@ -3,5 +3,18 @@ set -e
 
 export XZ_OPT=-9
 
-cd install/opt
-tar cJf build_linux_$QT_VER.tar.xz qt
+cd install/opt/qt/$QT_VER
+
+if [[ $EXCLUDE_PLATFORMS != *"linux"* ]]; then
+	tar cJf build_linux_$QT_VER.tar.xz gcc_64
+fi
+
+if [[ $EXCLUDE_PLATFORMS != *"android"* ]]; then
+	tar cJf build_android_$QT_VER.tar.xz android_armv7 android_x86
+fi
+
+cd ../Docs
+tar cJf build_doc_$QT_VER.tar.xz ./*
+
+cd ..
+find .
