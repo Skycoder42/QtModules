17f02b990d4b256adfc8e53b63846cd0eb9e3487
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-03 12:59:36 +0200

    improved doc generation

--- ci/linux/build.sh
+++ ci/linux/build.sh
@@ -8,19 +8,21 @@ export ANDROID_SDK_ROOT=$ANDROID_HOME
 export ANDROID_NDK_ROOT=$ANDROID_NDK
 
 scriptdir=$(dirname $0)
+needs_doc=0
 
 if [[ $EXCLUDE_PLATFORMS != *"linux"* ]]; then
 	$scriptdir/build-all.sh gcc_64 "QMAKE_CXX=g++-5"
+	if (( needs_doc == 0 )); then
+		$scriptdir/build-doc.sh gcc_64
+		needs_doc=1
+	fi
 fi
 
 if [[ $EXCLUDE_PLATFORMS != *"android"* ]]; then
 	$scriptdir/build-first.sh android_armv7
 	$scriptdir/build-first.sh android_x86
-fi
-
-if [[ $EXCLUDE_PLATFORMS != *"linux"* ]]; then
-	#generate docs
-	cd build-gcc_64/doc
-	make doxygen
-	make INSTALL_ROOT="$(pwd)/../../install" install
+	if (( needs_doc == 0 )); then
+		$scriptdir/build-doc.sh android_armv7
+		needs_doc=1
+	fi
 fi
