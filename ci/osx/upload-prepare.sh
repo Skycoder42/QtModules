93afe6af389a7f80a71b17be5546dedb8d1e9e43
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-03 13:32:40 +0200

    osx

--- ci/osx/upload-prepare.sh
+++ ci/osx/upload-prepare.sh
@@ -3,5 +3,8 @@ set -e
 
 export XZ_OPT=-9
 
-cd install/opt
-tar cJf build_osx_$QT_VER.tar.xz qt
+cd install/opt/qt/$QT_VER
+tar cJf build_osx_$QT_VER.tar.xz clang_64 ios
+
+cd ..
+find . -name "*.tar.xz"
