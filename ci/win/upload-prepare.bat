9408c52f63916306c082c2b199060eabc9d330ec
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-03 14:59:09 +0200

    split out mingw

--- ci/win/upload-prepare.bat
+++ ci/win/upload-prepare.bat
@@ -5,5 +5,6 @@ if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" (
 )
 
 if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" (
-	7z a build_win_msvc2015_%QT_VER%.zip msvc2015_64 msvc2015 mingw53_32
+	7z a build_win_msvc2015_%QT_VER%.zip msvc2015_64 msvc2015
+	7z a build_win_mingw_%QT_VER%.zip mingw53_32
 )
