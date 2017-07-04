423a1614d8d0b98bfe00c3912bf7fb0244127af9
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-04 18:09:08 +0200

    osx: opt tests

--- ci/osx/build.sh
+++ ci/osx/build.sh
@@ -3,10 +3,28 @@ set -e
 
 export MAKEFLAGS="-j$(sysctl -n hw.ncpu)"
 
+if [[ -z "$TEST_DIR" ]]; then
+	export TEST_DIR=./tests/auto
+fi
+
+olddir=$(pwd)
+for file in $(find . -name "qpm.json"); do
+	qpmdir=$(dirname $file)
+	if [[ "$qpmdir" != *"vendor"* ]]; then
+		cd $qpmdir
+		qpm install
+	fi
+done
+cd $olddir
+
 scriptdir=$(dirname $0)
 
 if [[ $EXCLUDE_PLATFORMS != *"mac"* ]]; then
-	$scriptdir/build-all.sh clang_64
+	if [[ -z "$NO_TESTS" ]]; then
+		$scriptdir/build-all.sh clang_64
+	else
+		$scriptdir/build-first.sh clang_64
+	fi
 fi
 
 if [[ $EXCLUDE_PLATFORMS != *"ios"* ]]; then
