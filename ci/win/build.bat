834aa432ad1f610bf64ba73ae3fe2613013d33b1
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-04 18:59:53 +0200

    fix all

--- ci/win/build.bat
+++ ci/win/build.bat
@@ -8,7 +8,6 @@ setlocal
 set olddir=%CD%
 for /R %%F in (*qpm.json) do (
 	echo %%F | findstr /C:"vendor" > nul || (
-		echo qpm dir is %%~dpF
 		cd %%~dpF
 		C:\projects\qpm.exe install
 		cd %olddir%
@@ -19,8 +18,7 @@ endlocal
 if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" (
 	set VC_DIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"
 
-	echo.%EXCLUDE_PLATFORMS% | findstr /C:"win32" 1>nul
-	if errorlevel 0 (
+	echo %EXCLUDE_PLATFORMS% | findstr /C:"win32" > nul || (
 		if "%NO_TESTS%" == "" (
 			call %~dp0\build-msvc-all.bat amd64 msvc2017_64 || exit /B 1
 		) else (
@@ -28,8 +26,7 @@ if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" (
 		)
 	)
 
-	echo.%EXCLUDE_PLATFORMS% | findstr /C:"winrt" 1>nul
-	if errorlevel 0 (
+	echo %EXCLUDE_PLATFORMS% | findstr /C:"winrt" > nul || (
 		call %~dp0\build-msvc-first.bat amd64 winrt_x64_msvc2017 || exit /B 1
 		call %~dp0\build-msvc-first.bat amd64_x86 winrt_x86_msvc2017 || exit /B 1
 		call %~dp0\build-msvc-first.bat amd64_arm winrt_armv7_msvc2017 || exit /B 1
@@ -39,8 +36,7 @@ if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" (
 if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" (
 	set VC_DIR="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 
-	echo.%EXCLUDE_PLATFORMS% | findstr /C:"win32" 1>nul
-	if errorlevel 0 (
+	echo %EXCLUDE_PLATFORMS% | findstr /C:"win32" > nul || (
 		if "%NO_TESTS%" == "" (
 			call %~dp0\build-msvc-all.bat amd64 msvc2015_64 || exit /B 1
 			call %~dp0\build-msvc-all.bat amd64_x86 msvc2015 || exit /B 1
