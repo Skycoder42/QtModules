89e02a9930d29cca210645b0a5d8d6b31554d4db
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-07 19:21:54 +0200

    linux build via docker for ubuntu 16!

--- ci/linux/setup.sh
+++ ci/linux/setup.sh
@@ -3,41 +3,12 @@ set -e
 
 scriptdir=$(dirname $0)
 
-# install build deps
-sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test 
-sudo apt-get -qq update
+echo FROM ubuntu:latest > $scriptdir/Dockerfile
+echo ENV QT_VER=$QT_VER >> $scriptdir/Dockerfile
+echo ENV PROJECT=$PROJECT >> $scriptdir/Dockerfile
+echo ENV EXCLUDE_PLATFORMS=$EXCLUDE_PLATFORMS >> $scriptdir/Dockerfile
+echo ENV TEST_DIR=$TEST_DIR >> $scriptdir/Dockerfile
+echo ENV NO_TESTS=$NO_TESTS >> $scriptdir/Dockerfile
+cat $scriptdir/Dockerfile-base >> $scriptdir/Dockerfile
 
-sudo apt-get -qq install --no-install-recommends libgl1-mesa-dev libglib2.0-0 libpulse-dev make git ca-certificates curl xauth libx11-xcb1 libfontconfig1 libdbus-1-3 g++-5 python3 doxygen openjdk-8-jdk
-
-# install qpm
-curl -Lo /tmp/qpm https://www.qpm.io/download/v0.10.0/linux_386/qpm
-sudo install -m 755 /tmp/qpm /usr/local/bin/
-
-# prepare installer script
-function test_include {
-	if [[ $EXCLUDE_PLATFORMS != *"$1"* ]]; then
-		echo true
-	else
-		echo false
-	fi
-}
-
-qtvid=$(echo $QT_VER | sed -e "s/\\.//g")
-echo pfLinux = \"$(test_include linux)\" > $scriptdir/qt-installer-script.qs
-echo pfAndroid = \"$(test_include android)\" >> $scriptdir/qt-installer-script.qs
-echo qtVersion = \"$qtvid\" >> $scriptdir/qt-installer-script.qs
-cat $scriptdir/qt-installer-script-base.qs >> $scriptdir/qt-installer-script.qs
-
-# install Qt
-curl -Lo /tmp/installer.run https://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run
-chmod +x /tmp/installer.run
-QT_QPA_PLATFORM=minimal sudo /tmp/installer.run --script $scriptdir/qt-installer-script.qs --addRepository https://install.skycoder42.de/qtmodules/linux_x64
-
-if [[ $EXCLUDE_PLATFORMS != *"android"* ]]; then
-	# android skd/ndk
-	curl -Lo /tmp/android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
-	mkdir $HOME/android
-	unzip -qq /tmp/android-sdk.zip -d $HOME/android/sdk/
-	echo y | $HOME/android/sdk/tools/bin/sdkmanager --update
-	echo y | $HOME/android/sdk/tools/bin/sdkmanager "platform-tools" "platforms;android-26" "build-tools;26.0.0" "extras;google;m2repository" "extras;android;m2repository" "ndk-bundle"
-fi
+sudo docker build -t skycoder42/qt-build $scriptdir
