bfab3e064833553a89d0cc5bf1700ba34b3629e6
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-07 13:06:10 +0200

    script setup complet

--- ci/win/replace.ps1
+++ ci/win/replace.ps1
@@ -1,2 +1,1 @@
-echo $args[0] $args[1]
-Get-Content $args[0] | ForEach-Object {$_ -replace "\.", ""} | Set-Content $args[0]
+Get-Content $args[0] | ForEach-Object {$_ -replace "\.", ""} | Set-Content $args[1]
--- ci/win/setup.bat
+++ ci/win/setup.bat
@@ -3,9 +3,10 @@
 :: powershell -Command "Invoke-WebRequest https://storage.googleapis.com/www.qpm.io/download/latest/windows_amd64/qpm.exe -OutFile C:\projects\qpm.exe"
 
 :: prepare installer script
-echo qtVersion = "%QT_VER%" > %~dp0\qt-installer-script.qs
-powershell -File %~dp0\replace.ps1 %~dp0\qt-installer-script.qs
+echo qtVersion = "%QT_VER%" > %~dp0\tmp.qs
+powershell -File %~dp0\replace.ps1 %~dp0\tmp.qs %~dp0\qt-installer-script.qs
 
+echo image = "%APPVEYOR_BUILD_WORKER_IMAGE%" >> %~dp0\qt-installer-script.qs
 call :test_include pfWin32 win32
 call :test_include pfWinrt winrt
 
@@ -19,8 +20,8 @@ C:\Qt\MaintenanceTool.exe --script ./qt-installer-modify-script.qs --addReposito
 
 :test_include
 echo %EXCLUDE_PLATFORMS% | findstr /C:"%2" > nul && (
-	echo %1 = "false" >> %~dp0\qt-installer-script.qs
+	echo %1 = false >> %~dp0\qt-installer-script.qs
 ) || (
-	echo %1 = "true" >> %~dp0\qt-installer-script.qs
+	echo %1 = true >> %~dp0\qt-installer-script.qs
 )
 exit /B 0
