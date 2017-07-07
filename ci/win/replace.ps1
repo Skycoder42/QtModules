c79f6d54635225e3c5940bba878806957ff1274e
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-07 12:55:09 +0200

    try with ps script

---
+++ ci/win/replace.ps1
@@ -0,0 +1,1 @@
+Get-Content %~dp0\qt-installer-script.qs | ForEach-Object {$_ -replace "\.", ""} | Set-Content %~dp0\qt-installer-script.qs
--- ci/win/setup.bat
+++ ci/win/setup.bat
@@ -3,13 +3,11 @@
 :: powershell -Command "Invoke-WebRequest https://storage.googleapis.com/www.qpm.io/download/latest/windows_amd64/qpm.exe -OutFile C:\projects\qpm.exe"
 
 :: prepare installer script
-echo qtVersion = ""%QT_VER%"" | call %~dp0\BatchSubstitute.bat "." "" > %~dp0\qt-installer-script.qs
+echo qtVersion = "%QT_VER%" > %~dp0\qt-installer-script.qs
+powershell -File %~dp0\replace.ps1
 
-echo pfWin32 = 
-call :test_include "win32"
-
-echo pfWinrt = 
-call :test_include "winrt"
+call :test_include "pfWin32" "win32"
+call :test_include "pfWinrt" "winrt"
 
 type %~dp0\qt-installer-modify-script.qs >> %~dp0\qt-installer-script.qs
 
@@ -20,9 +18,9 @@ C:\Qt\MaintenanceTool.exe --silentUpdate || exit \B 1
 C:\Qt\MaintenanceTool.exe --script ./qt-installer-modify-script.qs --addRepository https://install.skycoder42.de/qtmodules/windows_x86 || exit \B 1
 
 :test_include
-echo %EXCLUDE_PLATFORMS% | findstr /C:"%1" > nul && (
-	echo "false" >> %~dp0\qt-installer-script.qs
+echo %EXCLUDE_PLATFORMS% | findstr /C:"%2" > nul && (
+	echo %1 = "false" >> %~dp0\qt-installer-script.qs
 ) || (
-	echo "true" >> %~dp0\qt-installer-script.qs
+	echo %1 = "true" >> %~dp0\qt-installer-script.qs
 )
 exit /B 0
