174fc16dec0d9d424cab082000561693d6a53fc5
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-08 12:50:39 +0200

    fix build

--- ci/linux/build.sh
+++ ci/linux/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
-docker run --rm --name docker-qt-build -e PROJECT -e EXCLUDE_PLATFORMS -e TEST_DIR -e NO_TESTS -v "$(pwd):/root/project" skycoder42/qt-build
+qtvid=$(echo $QT_VER | sed -e "s/\\.//g")
+docker run --rm --name docker-qt-build -e PROJECT -e EXCLUDE_PLATFORMS -e TEST_DIR -e NO_TESTS -v "$(pwd):/root/project" skycoder42/qt-build:qt$qtvid
