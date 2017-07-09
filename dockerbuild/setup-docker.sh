61498c5d7b8d6a9134e8a9f25eeb69e606fb9be1
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-09 17:41:52 +0200

    use latest and install libjasper

--- dockerbuild/setup-docker.sh
+++ dockerbuild/setup-docker.sh
@@ -5,7 +5,7 @@ scriptdir=$(dirname $0)
 
 # install build deps
 apt-get -qq update
-apt-get -qq install --no-install-recommends libgl1-mesa-dev libglib2.0-0 libpulse-dev make g++ git ca-certificates curl xauth libx11-xcb1 libfontconfig1 libdbus-1-3 python3 doxygen openjdk-8-jdk unzip patchelf
+apt-get -qq install --no-install-recommends libgl1-mesa-dev libglib2.0-0 libpulse-dev make g++ git ca-certificates curl xauth libx11-xcb1 libfontconfig1 libdbus-1-3 python3 doxygen openjdk-8-jdk unzip patchelf libjasper1
 
 # install qpm
 curl -Lo /tmp/qpm https://www.qpm.io/download/v0.10.0/linux_386/qpm
