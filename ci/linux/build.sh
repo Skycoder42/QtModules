8785761868552eedb07ee0479c57ec6521286ad7
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-10 14:18:57 +0200

    -R

--- ci/linux/build.sh
+++ ci/linux/build.sh
@@ -4,4 +4,4 @@ set -e
 qtvid=$(echo $QT_VER | sed -e "s/\\.//g")
 docker run --rm --name docker-qt-build -e PROJECT -e EXCLUDE_PLATFORMS -e TEST_DIR -e NO_TESTS -v "$(pwd):/root/project" skycoder42/qt-build:qt$qtvid
 
-sudo chown -r $USER $(pwd)
+sudo chown -R $USER $(pwd)
