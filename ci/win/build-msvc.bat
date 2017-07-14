70b714faaa3f6366697783a6a39f26e701991828
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-15 00:14:14 +0200

    use gotos...

--- ci/win/build-msvc.bat
+++ ci/win/build-msvc.bat
@@ -15,24 +15,22 @@ nmake || exit /B 1
 nmake INSTALL_ROOT=\projects\%CurrDirName%\install install || exit /B 1
 
 :: build and run test
-if "%NO_TESTS%" == "" (
+if NOT "%NO_TESTS%" == "" goto no_tests
 	nmake all || exit /B 1
 
 	set PATH=C:\Qt\%QT_VER%\%qtplatform%\bin;%CD%\lib;%PATH%;
 	if "%TEST_DIR%" == "" (
 		set TEST_DIR=.\tests\auto
 	)
-)
-
 	cd %TEST_DIR%
 	set QT_QPA_PLATFORM=minimal
 	for /r %%f in (tst_*.exe) do (
 		%%f || exit /B 1
 	)
+:no_tests
 
-echo pre doc
 :: build documentation
-if NOT "%BUILD_DOC%" == "" (
+if "%BUILD_DOC%" == "" goto no_doc
 	echo pre doc real
 	cd \projects\%CurrDirName%
 	mkdir build-doc
@@ -41,4 +39,4 @@ if NOT "%BUILD_DOC%" == "" (
 	C:\Qt\%QT_VER%\%qtplatform%\bin\qmake -r ../doc/doc.pro || exit /B 1
 	nmake doxygen || exit /B 1
 	nmake INSTALL_ROOT=\projects\%CurrDirName%\install install || exit /B 1
-)
+:no_doc
