b457f2390dd2fbdf5fb7c8f5d68b45309564b688
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-14 23:35:31 +0200

    .

--- ci/win/build-msvc.bat
+++ ci/win/build-msvc.bat
@@ -14,8 +14,11 @@ C:\Qt\%QT_VER%\%qtplatform%\bin\qmake -r ..\%PROJECT%.pro || exit /B 1
 nmake || exit /B 1
 nmake INSTALL_ROOT=\projects\%CurrDirName%\install install || exit /B 1
 
-echo pre test
+echo pre test 1
 :: build and run test
+echo pre test 2
+if "true" == "false" echo pre test 3
+echo pre test 4 %NO_TESTS%
 if "%NO_TESTS%" == "" (
 	echo pre make all
 	nmake all || exit /B 1
