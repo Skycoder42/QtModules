ab0c7d6d449bde07da98073a79095810f95d0191
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-08-20 14:20:25 +0200

    fixup

--- deploy/unpack.sh
+++ deploy/unpack.sh
@@ -13,18 +13,19 @@ skip=$4
 # get the repogen script
 git clone "https://github.com/${repoId}" --branch "$version" ./gitrepo
 mv ./gitrepo/repogen.sh ./
+mv ./gitrepo/LICENSE ./
 rm -rf ./gitrepo
 
 #prepare dirs
 mkdir -p "$qtVer"
 mkdir -p archives
 
-pushd archives
+cd archives
 #download all possible packages (.tar.xz)
 for arch in android_armv7 android_x86 clang_64 doc gcc_64 ios; do
 	if [ "$skip" != *"$arch"* ]; then
-		file=build_${arch}_${qtDir}.tar.xz
-		wget "https://github.com/${repoId}/releases/download/${version}/$file"
+		file=build_${arch}_${qtVer}.tar.xz
+		wget -q "https://github.com/${repoId}/releases/download/${version}/$file"
 		tar -xf "$file" -C "../$qtVer/"
 	fi
 done
@@ -32,11 +33,11 @@ done
 #download all possible packages (.zip)
 for arch in mingw53_32 msvc2015 msvc2015_64 msvc2017_64 winrt_armv7_msvc2017 winrt_x64_msvc2017 winrt_x86_msvc2017; do
 	if [ "$skip" != *"$arch"* ]; then
-		file=build_${arch}_${qtDir}.zip
-		wget "https://github.com/${repoId}/releases/download/${version}/$file"
+		file=build_${arch}_${qtVer}.zip
+		wget -q "https://github.com/${repoId}/releases/download/${version}/$file"
 		unzip -qq "$file" -d "../$qtVer/"
 	fi
 done
-popd
+cd ..
 
 ./repogen.sh "$qtVer" "$version"
