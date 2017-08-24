ef95f4ce53187ee3e8a685febde74b73ada0724b
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-08-24 13:07:43 +0200

    updated script

--- deploy/unpack.sh
+++ deploy/unpack.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # $1 Qt Version
 # $2 repoid
 # $3 version
@@ -20,26 +20,30 @@ rm -rf ./gitrepo
 mkdir -p "$qtVer"
 mkdir -p archives
 
-cd archives
+pushd archives
 #download all possible packages (.tar.xz)
 for arch in android_armv7 android_x86 clang_64 doc gcc_64 ios; do
-	if [ "$skip" != *"$arch"* ]; then
-		file=build_${arch}_${qtVer}.tar.xz
-		echo downloading and extracting $file
-		wget -q "https://github.com/${repoId}/releases/download/${version}/$file"
-		tar -xf "$file" -C "../$qtVer/"
-	fi
+	for skip_pattern in $skip; do
+		if [[ "$arch" != *"$skip_pattern"* ]]; then
+			file=build_${arch}_${qtVer}.tar.xz
+			echo downloading and extracting $file
+			wget -q "https://github.com/${repoId}/releases/download/${version}/$file"
+			tar -xf "$file" -C "../$qtVer/"
+		fi
+	done
 done
 
 #download all possible packages (.zip)
 for arch in mingw53_32 msvc2015 msvc2015_64 msvc2017_64 winrt_armv7_msvc2017 winrt_x64_msvc2017 winrt_x86_msvc2017; do
-	if [ "$skip" != *"$arch"* ]; then
-		file=build_${arch}_${qtVer}.zip
-		echo downloading and extracting $file
-		wget -q "https://github.com/${repoId}/releases/download/${version}/$file"
-		unzip -qq "$file" -d "../$qtVer/"
-	fi
+	for skip_pattern in $skip; do
+		if [[ "$arch" != *"$skip_pattern"* ]]; then
+			file=build_${arch}_${qtVer}.zip
+			echo downloading and extracting $file
+			wget -q "https://github.com/${repoId}/releases/download/${version}/$file"
+			unzip -qq "$file" -d "../$qtVer/"
+		fi
+	done
 done
-cd ..
+popd
 
 ./repogen.sh "$qtVer" "$version"
