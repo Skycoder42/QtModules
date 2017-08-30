fed2d7dce93ba0a193b2c0d34d9b4e54554df7ce
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-08-30 23:04:32 +0200

    v2

--- ci/qt-static/build-qt.sh
+++ ci/qt-static/build-qt.sh
@@ -7,18 +7,14 @@ mkdir -p $tDir
 $SUDO chown -R $USER /opt/qt/$QT_VER/Src
 cd /opt/qt/$QT_VER/Src/
 
-for mod in qtbase $STATIC_QT_MODS; do
-	pushd $mod
-	if [[ "$mod" == "qtbase" ]]; then
-		./configure -prefix $tDir -opensource -confirm-license -release -static -static-runtime -no-cups -no-qml-debug -no-opengl -no-egl -no-xinput2 -no-sm -no-icu -nomake examples -nomake tests -accessibility -no-gui -no-widgets
-	else
-		../qtbase/bin/qmake -r
+for mod in $(ls -d qt*/ | cut -f1 -d'/'); do
+	if [[ "qtbase $STATIC_QT_MODS" != *"$mod"* ]]; then
+		skipPart="-skip $mod $skipPart"
 	fi
-	
-	make > /dev/null
-	$SUDO make install > /dev/null
-	
-	popd
 done
+
+./configure -prefix $tDir -opensource -confirm-license -release -static -static-runtime -no-cups -no-qml-debug -no-opengl -no-egl -no-xinput2 -no-sm -no-icu -nomake examples -nomake tests -accessibility -no-gui -no-widgets $skipPart
+make > /dev/null
+$SUDO make install > /dev/null
 
 find /tmp/qt-static
