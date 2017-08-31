75617e5ddb4ed7a78864861452ab19fe636cf22e
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-08-31 17:33:33 +0200

    dbg

--- ci/win/setup-qt-static.bat
+++ ci/win/setup-qt-static.bat
@@ -20,9 +20,12 @@ for /D %%G in (*) do (
 
 call %VC_DIR% amd64 || exit /B 1
 
-.\configure -prefix %tDir% -opensource -confirm-license -release -static -static-runtime -no-cups -no-qml-debug -no-opengl -no-egl -no-xinput2 -no-sm -no-icu -nomake examples -nomake tests -accessibility -no-gui -no-widgets %skipPart% || exit /B 1
-nmake || exit /B 1
-nmake install || exit /B 1
+echo before config
+.\configure -prefix %tDir% -opensource -confirm-license -release -static -static-runtime -no-cups -no-qml-debug -no-opengl -no-egl -no-xinput2 -no-sm -no-icu -nomake examples -nomake tests -accessibility -no-gui -no-widgets %skipPart%
+echo before make
+nmake
+echo before make install
+nmake install
 
 cd ../static
 dir
