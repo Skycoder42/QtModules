2a2cf7f419a24164bdb7943f3de4515bfc8dfe4d
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-08-31 11:04:51 +0200

    get sources via maintenancetool

--- ci/win/setup.bat
+++ ci/win/setup.bat
@@ -6,11 +6,6 @@ echo %PLATFORM% | findstr /C:"winrt" > nul || (
 	set EXTRA_MODULES=qt.tools.ifw.20;%EXTRA_MODULES%
 )
 
-:: build static qt
-if "%STATIC_TOOLS%" == "" goto no_static
-	%~dp0\setup-qt-static.bat || exit \B 1
-:no_static
-
 :: prepare installer script
 echo qtVersion = "%QT_VER%"; > %~dp0\tmp.qs
 powershell -File %~dp0\replace.ps1 %~dp0\tmp.qs %~dp0\qt-installer-script.qs
@@ -25,6 +20,7 @@ if "%PLATFORM%" == "mingw53_32" (
 	set PACKAGE=win32_mingw53
 	set EXTRA_MODULES=qt.tools.win32_mingw530;%EXTRA_MODULES%
 )
+if "%PLATFORM%" == "static" set PACKAGE=src
 
 echo platform = "%PACKAGE%"; >> %~dp0\qt-installer-script.qs
 echo extraMods = []; >> %~dp0\qt-installer-script.qs
@@ -37,4 +33,9 @@ type %~dp0\qt-installer-modify-script.qs >> %~dp0\qt-installer-script.qs
 C:\Qt\MaintenanceTool.exe --silentUpdate || exit \B 1
 C:\Qt\MaintenanceTool.exe --script %~dp0\qt-installer-script.qs --addRepository https://install.skycoder42.de/qtmodules/windows_x86 || (
 	find /c "no_modules_changed" C:\Qt\InstallationLog.txt > nul || exit \B 1
+)
+
+:: build static qt
+if "%PLATFORM%" == "static" (
+	%~dp0\setup-qt-static.bat || exit \B 1
 )
