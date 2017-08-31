bfb02291e7e9292b2327386e1e7fcb447f608365
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-08-31 10:54:48 +0200

    Revert "downloads sources and build"
    This reverts commit 3999b9940e56dee5cbe326c695285676bee38b4e.

--- ci/linux/setup-qt-static.sh
+++ ci/linux/setup-qt-static.sh
@@ -1,35 +1,22 @@
 #!/bin/bash
 set -e
 
-if [[ -z "$QT_VER_SHORT" ]]; then
-	QT_VER_SHORT=$QT_VER
-fi
-if [[ -z "$QT_VER_FULL" ]]; then
-	QT_VER_FULL=${QT_VER_SHORT}.0
-fi
-
 tDir=/opt/qt-static
 mkdir -p $tDir
 
+$SUDO chown -R $USER /opt/qt/$QT_VER/Src
+cd /opt/qt/$QT_VER/Src/
 pushd $(mktemp -d)
 mkdir src
 mkdir download
 
-pushd download
-for mod in qtbase $STATIC_QT_MODS; do
-	curl -Lo "pkg-$mod.tar.xz" "https://download.qt.io/official_releases/qt/$QT_VER_SHORT/$QT_VER_FULL/submodules/$mod-opensource-src-$QT_VER_FULL.tar.xz"
-	tar -xf "pkg-$mod.tar.xz"
-	mv "$mod"* ./src/$mod
+for mod in $(ls -d qt*/ | cut -f1 -d'/'); do
+	if [[ "qtbase $STATIC_QT_MODS" != *"$mod"* ]]; then
+		skipPart="-skip $mod $skipPart"
+	fi
 done
-popd
-rm -rf download
 
-cd src
-
-pushd qtbase
-./qtbase/configure -top-level -prefix $tDir -opensource -confirm-license -release -static -static-runtime -no-cups -no-qml-debug -no-opengl -no-egl -no-xinput2 -no-sm -no-icu -nomake examples -nomake tests -accessibility -no-gui -no-widgets $skipPart
-popd
-
+./configure -prefix $tDir -opensource -confirm-license -release -static -static-runtime -no-cups -no-qml-debug -no-opengl -no-egl -no-xinput2 -no-sm -no-icu -nomake examples -nomake tests -accessibility -no-gui -no-widgets $skipPart
 make > /dev/null
 $SUDO make install > /dev/null
 
