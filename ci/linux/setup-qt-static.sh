640b18939804981cfb8ea21a77e552e8371c10db
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-09-03 10:45:47 +0200

    try explicit build

--- ci/linux/setup-qt-static.sh
+++ ci/linux/setup-qt-static.sh
@@ -9,19 +9,29 @@ if [[ -n "$SUDO" ]]; then
 fi
 pushd /opt/qt/$QT_VER/Src/
 
+#bug: remove gui from macdeployqt
+echo "QT -= gui" >> qttools/src/macdeployqt/macdeployqt/macdeployqt.pro
+echo "QT -= gui" >> qttools/src/macdeployqt/macchangeqt/macchangeqt.pro
+
+#build modules explicitly
+for mod in qtbase $STATIC_QT_MODS; do
+	pushd $mod
+	if [[ "$mod" == "qtbase" ]]; then
+		./configure -prefix $tDir -opensource -confirm-license -release -static -static-runtime -no-use-gold-linker -no-cups -no-qml-debug -no-opengl -no-egl -no-xinput2 -no-sm -no-icu -nomake examples -nomake tests -accessibility -no-gui -no-widgets
+	else
+		$tDir/qmake -r
+	fi
+	make > /dev/null
+	$SUDO make install > /dev/null
+	popd
+done
+
+
 for mod in $(ls -d qt*/ | cut -f1 -d'/'); do
 	if [[ "qtbase $STATIC_QT_MODS" != *"$mod"* ]]; then
 		skipPart="-skip $mod $skipPart"
 	fi
 done
-
-#bug: remove gui from macdeployqt
-echo "QT -= gui" >> qttools/src/macdeployqt/macdeployqt/macdeployqt.pro
-echo "QT -= gui" >> qttools/src/macdeployqt/macchangeqt/macchangeqt.pro
-
-./configure -prefix $tDir -opensource -confirm-license -release -static -static-runtime -no-use-gold-linker -no-cups -no-qml-debug -no-opengl -no-egl -no-xinput2 -no-sm -no-icu -nomake examples -nomake tests -accessibility -no-gui -no-widgets $skipPart
-make > /dev/null
-$SUDO make install > /dev/null
 
 popd
 $SUDO rm -rf /opt/qt/$QT_VER/Src
--- ci/win/setup-qt-static.bat
+++ ci/win/setup-qt-static.bat
@@ -8,23 +8,22 @@ if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" (
 if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" (
 	set VC_DIR="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 )
-	
+
+call %VC_DIR% amd64 || exit /B 1
+
 set tDir=C:\Qt\%QT_VER%\static
 mkdir -p %tDir% || exit /B 1
 
 cd C:\Qt\%QT_VER%\Src
-set skipPart=
-for /D %%G in (qt*) do (
-	echo "qtbase %STATIC_QT_MODS%" | findstr /C:"%%G" > nul || (
-		set skipPart=-skip %%G !skipPart!
+
+for %%m in (qtbase;%STATIC_QT_MODS%) do (
+	cd %%m
+	if "%%m" == "qtbase" (
+		call .\configure -prefix %tDir% -platform win32-msvc -opensource -confirm-license -release -static -static-runtime -no-cups -no-qml-debug -no-opengl -no-egl -no-xinput2 -no-sm -no-icu -nomake examples -nomake tests -accessibility -no-gui -no-widgets || exit /B 1
+	) else (
+		%tDir%\qmake -r
 	)
+	nmake > nul || exit /B 1
+	nmake install > nul || exit /B 1
+	cd ..
 )
-
-call %VC_DIR% amd64 || exit /B 1
-
-call .\configure -prefix %tDir% -platform win32-msvc -opensource -confirm-license -release -static -static-runtime -no-cups -no-qml-debug -no-opengl -no-egl -no-xinput2 -no-sm -no-icu -nomake examples -nomake tests -accessibility -no-gui -no-widgets %skipPart% || exit /B 1
-nmake > nul || exit /B 1
-nmake install > nul || exit /B 1
-
-cd ../static
-dir
