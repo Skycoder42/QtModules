fcb164373c7e9217d90a4de7540f1131e2703bf6
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-09-03 13:53:31 +0200

    seperate builds

--- ci/linux/setup-qt-static.sh
+++ ci/linux/setup-qt-static.sh
@@ -13,24 +13,25 @@ pushd /opt/qt/$QT_VER/Src/
 echo "QT -= gui" >> qttools/src/macdeployqt/macdeployqt/macdeployqt.pro
 echo "QT -= gui" >> qttools/src/macdeployqt/macchangeqt/macchangeqt.pro
 
-#build modules explicitly
-for mod in qtbase $STATIC_QT_MODS; do
-	pushd $mod
-	if [[ "$mod" == "qtbase" ]]; then
-		./configure -prefix $tDir -opensource -confirm-license -release -static -static-runtime -no-use-gold-linker -no-cups -no-qml-debug -no-opengl -no-egl -no-xinput2 -no-sm -no-icu -nomake examples -nomake tests -accessibility -no-gui -no-widgets
-	else
-		$tDir/bin/qmake -r
-	fi
-	make > /dev/null
-	$SUDO make install > /dev/null
-	popd
-done
-
-
+# generate skip modules
 for mod in $(ls -d qt*/ | cut -f1 -d'/'); do
 	if [[ "qtbase $STATIC_QT_MODS" != *"$mod"* ]]; then
 		skipPart="-skip $mod $skipPart"
 	fi
+done
+
+# build qt
+./configure -prefix $tDir -opensource -confirm-license -release -static -static-runtime -no-use-gold-linker -no-cups -no-qml-debug -no-opengl -no-egl -no-xinput2 -no-sm -no-icu -nomake examples -nomake tests -accessibility -no-gui -no-widgets $skipPart
+make > /dev/null
+$SUDO make install > /dev/null
+
+#build extra modules explicitly
+for mod in $STATIC_EXTRA_MODS; do
+	pushd $mod
+	$tDir/qmake -r
+	make > /dev/null
+	$SUDO make install > /dev/null
+	popd
 done
 
 popd
