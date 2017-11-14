099d3b52d9ff5069af411781a14251869ce85387
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-11-14 23:48:00 +0100

    name != nmake...

--- ci/win/build-msvc.bat
+++ ci/win/build-msvc.bat
@@ -10,7 +10,7 @@ mkdir build-%qtplatform%
 cd build-%qtplatform%
 
 C:\Qt\%QT_VER%\%qtplatform%\bin\qmake ../ || exit /B 1
-name qmake_all || exit /B 1
+nmake qmake_all || exit /B 1
 nmake || exit /B 1
 nmake INSTALL_ROOT=\projects\%CurrDirName%\install install || exit /B 1
 
