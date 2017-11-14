39bb3fa9d88fe564c259e37126f022cae50b1913
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-11-15 00:04:44 +0100

    sudo again

--- ci/linux/setup/setup-android.sh
+++ ci/linux/setup/setup-android.sh
@@ -4,7 +4,7 @@ set -e
 scriptdir=$(dirname $0)
 
 # install android deps
-sudo apt-get -qq install --no-install-recommends openjdk-8-jdk unzip
+apt-get -qq install --no-install-recommends openjdk-8-jdk unzip
 
 # android skd/ndk
 curl -Lo /tmp/android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
