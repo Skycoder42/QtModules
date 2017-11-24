78e428f7c411e3159d0724490ea7445dec746bcf
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-11-24 22:11:00 +0100

    log

--- ci/win/upload-prepare.bat
+++ ci/win/upload-prepare.bat
@@ -2,7 +2,9 @@ cd install\Qt\%QT_VER%
 
 if "%PLATFORM%" == "static" (
 	set PLATFORM=static_win
-	rename .\static %PLATFORM%
+	dir
+	rename static %PLATFORM%
+	dir
 )
 
 7z a build_%PLATFORM%_%QT_VER%.zip %PLATFORM%
