77acd0b3221a773284552dc05d13f3a5007f990f
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-11-25 01:46:19 +0100

    dsfsdefaefr

--- ci/win/upload-prepare.bat
+++ ci/win/upload-prepare.bat
@@ -2,9 +2,13 @@ cd install\Qt\%QT_VER%
 
 if "%PLATFORM%" == "static" (
 	set PLATFORM=static_win
-	move static %PLATFORM%
+	echo %PLATFORM%
+	dir
+	move "static" "%PLATFORM%" || exit \B 1
 	dir
 )
 
+dir
+echo %PLATFORM%
 7z a build_%PLATFORM%_%QT_VER%.zip %PLATFORM%
 move build_%PLATFORM%_%QT_VER%.zip ..\..\
