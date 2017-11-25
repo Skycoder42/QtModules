36b1e491e5b1997a81169e130b07891b37c197d6
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-11-25 14:42:37 +0100

    wtf

--- ci/win/upload-prepare.bat
+++ ci/win/upload-prepare.bat
@@ -1,12 +1,10 @@
 cd install\Qt\%QT_VER%
 
 if "%PLATFORM%" == "static" (
-	set PLATFORM=static_win
-	robocopy static %PLATFORM% /e /s || exit \B 1
-	dir
+	rename static static_win || exit \B 1
+	7z a build_static_win_%QT_VER%.zip static_win || exit \B 1
+	move build_static_win_%QT_VER%.zip ..\..\ || exit \B 1
+) else (
+	7z a build_%PLATFORM%_%QT_VER%.zip %PLATFORM% || exit \B 1
+	move build_%PLATFORM%_%QT_VER%.zip ..\..\ || exit \B 1
 )
-
-dir
-echo %PLATFORM%
-7z a build_%PLATFORM%_%QT_VER%.zip %PLATFORM%
-move build_%PLATFORM%_%QT_VER%.zip ..\..\
