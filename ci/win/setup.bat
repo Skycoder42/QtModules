f37bd655478a5f58d56367be7c33f626c838149a
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-12-13 15:32:34 +0100

    skip qt update

--- ci/win/setup.bat
+++ ci/win/setup.bat
@@ -33,7 +33,7 @@ for %%x in (%EXTRA_MODULES%) do (
 type %~dp0\qt-installer-modify-script.qs >> %~dp0\qt-installer-script.qs
 
 :: update and install Qt modules
-C:\Qt\MaintenanceTool.exe --silentUpdate || exit \B 1
+:: TODO C:\Qt\MaintenanceTool.exe --silentUpdate || exit \B 1
 C:\Qt\MaintenanceTool.exe --script %~dp0\qt-installer-script.qs --addRepository https://install.skycoder42.de/qtmodules/windows_x86 || (
 	find /c "no_modules_changed" C:\Qt\InstallationLog.txt > nul || (
 		:: print last 100 lines of install log
