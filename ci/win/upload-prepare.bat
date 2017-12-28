97205b7f4f093693c9bf2c1539aebf52455e83f5
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-12-28 19:28:22 +0100

    fix upload

--- ci/win/upload-prepare.bat
+++ ci/win/upload-prepare.bat
@@ -1,10 +1,10 @@
-cd install\Qt\%QT_VER%
+cd install\projects\Qt\%QT_VER%
 
 if "%PLATFORM%" == "static" (
 	rename static static_win || exit \B 1
 	7z a build_static_win_%QT_VER%.zip static_win || exit \B 1
-	move build_static_win_%QT_VER%.zip ..\..\ || exit \B 1
+	move build_static_win_%QT_VER%.zip ..\..\..\ || exit \B 1
 ) else (
 	7z a build_%PLATFORM%_%QT_VER%.zip %PLATFORM% || exit \B 1
-	move build_%PLATFORM%_%QT_VER%.zip ..\..\ || exit \B 1
+	move build_%PLATFORM%_%QT_VER%.zip ..\..\..\ || exit \B 1
 )
