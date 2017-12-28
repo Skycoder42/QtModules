af21c8efe5232c3506515899b56748730e6abd8b
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-12-28 13:17:31 +0100

    temp repo?

--- ci/win/setup.bat
+++ ci/win/setup.bat
@@ -34,7 +34,7 @@ type %~dp0\qt-installer-modify-script.qs >> %~dp0\qt-installer-script.qs
 
 :: update and install Qt modules
 :: C:\Qt\MaintenanceTool.exe --silentUpdate || exit \B 1
-C:\Qt\MaintenanceTool.exe --script %~dp0\qt-installer-script.qs --addRepository https://install.skycoder42.de/qtmodules/windows_x86 || exit \B 1
+C:\Qt\MaintenanceTool.exe --script %~dp0\qt-installer-script.qs --addTempRepository https://install.skycoder42.de/qtmodules/windows_x86 || exit \B 1
 
 :: build static qt
 if "%PLATFORM%" == "static" (
