5a72730145d43b3d2ab8e8c93a3bec723370e29e
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-02-05 18:10:24 +0100

    qpm via brew

--- ci/osx/setup.sh
+++ ci/osx/setup.sh
@@ -7,11 +7,7 @@ scriptdir=$(dirname $0)
 brew update
 brew tap Skycoder42/qt-modules
 brew upgrade coreutils || brew install coreutils
-brew install python3 qpmx $EXTRA_PKG
-
-# install qpm
-curl -Lo /tmp/qpm https://storage.googleapis.com/www.qpm.io/download/latest/darwin_386/qpm
-sudo install -m 755 /tmp/qpm /usr/local/bin/
+brew install python3 qpmx qpm $EXTRA_PKG
 
 # clang only -> install qtifw
 if [[ $PLATFORM == "clang_64" ]]; then
