9917a93dbcd7048048f4740cecbca5ff84ffc97b
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-02-06 18:41:05 +0100

    cleanup

--- deploy/repogen.py
+++ deploy/repogen.py
@@ -127,19 +127,18 @@ def url_extract(sdir, url, as_zip=False):
 			archive.extractall(sdir)
 
 
-def prepare_sources(sdir, repo, mod_name, vers):
-	print("Downloading an preparing sources")
+def prepare_sources(tdir, repo, mod_name, vers):
+	print("  -> Downloading source tarball")
 	# download sources
+	out_dir = pjoin(tdir, mod_name + "-" + vers)
 	mod_url = "https://github.com/" + repo + "/archive/" + vers + ".tar.gz"
-	out_dir = pjoin(sdir, "Src", mod_name.lower())
-	url_extract(sdir, mod_url)
-	shutil.move(pjoin(sdir, mod_name + "-" + vers), out_dir)
+	url_extract(tdir, mod_url)
 
 	# prepare sources
 	os.remove(pjoin(out_dir, ".travis.yml"))
 	os.remove(pjoin(out_dir, "appveyor.yml"))
 	# DEBUG shutil.move(pjoin(out_dir, "deploy.json"), pjoin(sdir, "deploy.json"))
-	shutil.copy2("/home/sky/Programming/QtLibraries/QtDataSync/deploy.json", pjoin(sdir, "deploy.json"))
+	shutil.copy2("/home/sky/Programming/QtLibraries/QtDataSync/deploy.json", pjoin(tdir, "deploy.json"))
 
 	return out_dir
 
@@ -151,39 +150,14 @@ def prepare_headers(tdir, mdir, mods, version):
 		file.write(syncqt_req.content)
 	os.chmod(syncqt_pl, 0o755)
 	for mod in mods:
+		print("  -> Creating public includes for " + mod)
 		subprocess.run([
 			syncqt_pl,
 			"-module", mod,
 			"-version", version,
 			"-outdir", mdir,
 			mdir
-		], check=True)
-
-
-def get_binary_imp(sdir, repo, vers, qt_vers, excludes, arch, as_zip):
-	for exclude in excludes:
-		if exclude in arch:
-			return False
-
-	bin_url="https://github.com/" + repo + "/releases/download/" + vers + "/build_" + arch + "_" + qt_vers
-	if as_zip:
-		bin_url += ".zip"
-	else:
-		bin_url += ".tar.xz"
-	print("Downloading and extracting " + arch)
-	url_extract(sdir, bin_url, as_zip)
-	return True
-
-
-def get_binaries(sdir, repo, vers, qt_vers, excludes):
-	use_arch = []
-	for arch in ["android_armv7", "android_x86", "clang_64", "doc", "gcc_64", "ios", "static_linux", "static_osx"]:
-		if get_binary_imp(sdir, repo, vers, qt_vers, excludes, arch, False):
-			use_arch.append(arch)
-	for arch in ["mingw53_32", "msvc2015", "msvc2015_64", "msvc2017_64", "winrt_armv7_msvc2017", "winrt_x64_msvc2017", "winrt_x86_msvc2017", "static_win"]:
-		if get_binary_imp(sdir, repo, vers, qt_vers, excludes, arch, True):
-			use_arch.append(arch)
-	return use_arch
+		], check=True, stdout=subprocess.DEVNULL)
 
 
 def pkg_meta(pdir):
@@ -220,10 +194,11 @@ def pkg_copy_data(pdir, sdir, arch, qt_version):
 	shutil.copytree(src_dir, target_dir, symlinks=True)
 
 
-def create_base_pkg(rdir, msdir, pkg_base, config, version, qt_version):
-	print("Creating meta package")
+def create_base_pkg(rdir, sdir, pkg_base, config, version, qt_version):
+	print("=> Creating meta package")
 	pkg_dir = pkg_prepare(rdir, pkg_base)
 
+	print("  -> Create meta data")
 	depends = []
 	for dep in config["dependencies"]:
 		if dep[0] == ".":
@@ -240,15 +215,16 @@ def create_base_pkg(rdir, msdir, pkg_base, config, version, qt_version):
 						datetime.date.today(),
 						config["license"]["name"])
 
-	shutil.copy2(pjoin(msdir, config["license"]["path"]),
+	shutil.copy2(pjoin(sdir, config["license"]["path"]),
 				 pjoin(pkg_meta(pkg_dir), "LICENSE.txt"))
 
 
 def create_src_pkg(rdir, sdir, pkg_base, config, version, qt_version):
-	print("Creating source package")
+	print("=> Creating source package")
 	pkg_src = pkg_base + ".src"
 	pkg_dir = pkg_prepare(rdir, pkg_src)
 
+	print("  -> Creating meta data")
 	pkg_qt_src = "qt.qt5.{}.src".format(qt_vid(qt_version))
 	pkg_add_package_xml(pkg_dir, pkg_src_xml,
 						pkg_base,
@@ -259,11 +235,13 @@ def create_src_pkg(rdir, sdir, pkg_base, config, version, qt_version):
 						pkg_qt_src,
 						pkg_qt_src)
 
-	pkg_copy_data(pkg_dir, sdir, "Src", qt_version)
+	print("  -> Moving sources")
+	src_base_dir = pjoin(pkg_data(pkg_dir), qt_version, "Src")
+	os.makedirs(src_base_dir, exist_ok=True)
+	shutil.move(sdir, pjoin(src_base_dir, config["title"].lower()))
 
 
-def create_doc_pkg(rdir, sdir, pkg_base, config, version, qt_version):
-	print("Creating doc package")
+def create_doc_meta(rdir, pkg_base, config, version, qt_version):
 	pkg_doc = pkg_base + ".doc"
 	pkg_dir = pkg_prepare(rdir, pkg_doc)
 
@@ -277,13 +255,10 @@ def create_doc_pkg(rdir, sdir, pkg_base, config, version, qt_version):
 						pkg_qt_doc,
 						pkg_qt_doc)
 	pkg_add_script(pkg_dir, pkg_doc_qs, qt_version)
-
-	shutil.copytree(pjoin(sdir, "Qt-" + qt_version),
-					pjoin(pkg_data(pkg_dir), "Doc", "Qt-" + qt_version),
-					symlinks=True)
+	return pkg_dir
 
 
-def create_arch_pkg(rdir, sdir, pkg_base, arch, config, version, qt_version):
+def create_arch_meta(rdir, pkg_base, arch, config, version, qt_version):
 	embedded_keys = [
 		"android_armv7",
 		"android_x86",
@@ -293,7 +268,6 @@ def create_arch_pkg(rdir, sdir, pkg_base, arch, config, version, qt_version):
 		"winrt_armv7_msvc2017"
 	]
 
-	print("Creating " + arch + " package")
 	pkg_arch = pkg_base + "." + arch
 	pkg_dir = pkg_prepare(rdir, pkg_arch)
 
@@ -312,8 +286,39 @@ def create_arch_pkg(rdir, sdir, pkg_base, arch, config, version, qt_version):
 				   qt_version,
 				   arch,
 				   "emb-arm-qt5" if arch in embedded_keys else "qt5")
+	return pkg_dir
 
-	pkg_copy_data(pkg_dir, sdir, arch, qt_version)
+
+def create_bin_pkg(rdir, pkg_base, repo, arch, config, version, qt_version, as_zip):
+	for exclude in config["excludes"]:
+		if exclude in arch:
+			return
+
+	print("=> Creating " + arch + " package")
+
+	# prepare metadata
+	print("  -> Creating meta data")
+	if arch == "doc":
+		pkg_dir = create_doc_meta(rdir, pkg_base, config, version, qt_version)
+	else:
+		pkg_dir = create_arch_meta(rdir, pkg_base, arch, config, version, qt_version)
+
+	# download sources
+	print("  -> Downloading and extracting data from github")
+	bin_url = "https://github.com/" + repo + "/releases/download/" + version + "/build_" + arch + "_" + qt_version
+	bin_url += ".zip" if as_zip else ".tar.xz"
+	url_extract(pjoin(pkg_data(pkg_dir), "Docs" if arch == "doc" else qt_version),
+				bin_url,
+				as_zip)
+
+
+def create_all_pkgs(rdir, pkg_base, repo, config, version, qt_version):
+	# tar packages
+	for arch in ["android_armv7", "android_x86", "clang_64", "doc", "gcc_64", "ios", "static_linux", "static_osx"]:
+		create_bin_pkg(rdir, pkg_base, repo, arch, config, version, qt_version, False)
+	# zip packages
+	for arch in ["mingw53_32", "msvc2015", "msvc2015_64", "msvc2017_64", "winrt_armv7_msvc2017", "winrt_x64_msvc2017", "winrt_x86_msvc2017", "static_win"]:
+		create_bin_pkg(rdir, pkg_base, repo, arch, config, version, qt_version, True)
 
 
 def repogen(repo_id, version, qt_version):
@@ -327,29 +332,23 @@ def repogen(repo_id, version, qt_version):
 		shutil.rmtree(tmp_dir, ignore_errors=True)
 		os.mkdir(tmp_dir)
 
-		# general
-		src_dir = pjoin(tmp_dir, "src")
-		os.mkdir(src_dir)
-
-		# step 1: download and prepare sources, download binaries
-		mod_src_dir = prepare_sources(src_dir, repo_id, mod_name, version)
-		with open(pjoin(src_dir, "deploy.json")) as file:
+		# step 1: download and prepare sources
+		print("=> Downloading an preparing sources")
+		src_dir = prepare_sources(tmp_dir, repo_id, mod_name, version)
+		with open(pjoin(tmp_dir, "deploy.json")) as file:
 			config = json.load(file)
-		prepare_headers(src_dir, mod_src_dir, cfg_if(config, "modules", mod_name), version)
-		use_arch = get_binaries(src_dir, repo_id, version, qt_version, cfg_if(config, "excludes", []))
+		prepare_headers(tmp_dir, src_dir, cfg_if(config, "modules", mod_name), version)
 
-		# step 2: create the repositories
+		# step 2: create the meta and src repositories
 		rep_dir = pjoin(tmp_dir, "repos")
 		os.mkdir(rep_dir)
 		pkg_base = "qt.qt5.{}.{}.{}".format(qt_vid(qt_version), user.lower(), mod_title.lower())
-		create_base_pkg(rep_dir, mod_src_dir, pkg_base, config, version, qt_version)
+		create_base_pkg(rep_dir, src_dir, pkg_base, config, version, qt_version)
 		if "Src" not in config["excludes"]:
 			create_src_pkg(rep_dir, src_dir, pkg_base, config, version, qt_version)
-		for arch in use_arch:
-			if arch == "doc":
-				create_doc_pkg(rep_dir, src_dir, pkg_base, config, version, qt_version)
-			else:
-				create_arch_pkg(rep_dir, src_dir, pkg_base, arch, config, version, qt_version)
+
+		# step 3: download and add binary repositories
+		create_all_pkgs(rep_dir, pkg_base, repo_id, config, version, qt_version)
 
 
 if __name__ == '__main__':
