4321e3d377029b8692e81f043c6af4ad969f7863
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-02-14 15:30:57 +0100

    verbose installer...

--- ci/win/setup.bat
+++ ci/win/setup.bat
@@ -34,7 +34,7 @@ type %~dp0\qt-installer-script-base.qs >> %~dp0\qt-installer-script.qs
 
 :: update and install Qt modules
 powershell -Command "Invoke-WebRequest https://download.qt.io/official_releases/online_installers/qt-unified-windows-x86-online.exe -OutFile C:\projects\qtinst.exe"
-C:\projects\qtinst.exe --script %~dp0\qt-installer-script.qs --addTempRepository https://install.skycoder42.de/qtmodules/windows_x86
+C:\projects\qtinst.exe --script %~dp0\qt-installer-script.qs --addTempRepository https://install.skycoder42.de/qtmodules/windows_x86 --verbose
 
 :: build static qt
 if "%PLATFORM%" == "static" (
