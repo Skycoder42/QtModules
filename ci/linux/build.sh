5d9b719d337f233904d0a95615d4c75114766148
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-03-08 13:49:33 +0100

    missing docker export

--- ci/linux/build.sh
+++ ci/linux/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-docker run --rm --name docker-qt-build -e QMAKE_FLAGS -e BUILD_DOC -e TEST_DIR -e NO_TESTS -v "$(pwd):/root/project" skycoder42/qt-build
+docker run --rm --name docker-qt-build -e QMAKE_FLAGS -e BUILD_DOC -e BUILD_EXAMPLES -e TEST_DIR -e NO_TESTS -v "$(pwd):/root/project" skycoder42/qt-build
 sudo chown -R $USER $(pwd)
