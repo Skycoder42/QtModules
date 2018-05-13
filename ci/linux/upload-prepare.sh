d0cf27fc29ae45d741d4c3dde5cf15389e1fff82
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-05-13 18:16:55 +0200

    better uploads

--- ci/linux/upload-prepare.sh
+++ ci/linux/upload-prepare.sh
@@ -11,16 +11,16 @@ if [[ $PLATFORM == "static" ]]; then
 fi
 
 tar cJf build_${PLATFORM}_${QT_VER}.tar.xz $PLATFORM
-mv build_${PLATFORM}_${QT_VER}.tar.xz ../../
+mv build_${PLATFORM}_${QT_VER}.tar.xz ../../../
 
 if [[ -n "$BUILD_DOC" ]]; then
 	cd ../Docs
 	tar cJf build_doc_$QT_VER.tar.xz ./*
-	mv build_doc_$QT_VER.tar.xz ../../
+	mv build_doc_$QT_VER.tar.xz ../../../
 fi
 
 if [[ -n "$BUILD_EXAMPLES" ]]; then
 	cd ../Examples
 	tar cJf build_examples_$QT_VER.tar.xz ./*
-	mv build_examples_$QT_VER.tar.xz ../../
+	mv build_examples_$QT_VER.tar.xz ../../../
 fi
