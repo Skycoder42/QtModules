3759727bcc29ce7dc62e164613438097b9c723fd
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-06-01 19:13:56 +0200

    extended fail log

--- ci/win/build-mingw.bat
+++ ci/win/build-mingw.bat
@@ -27,8 +27,9 @@ if NOT "%NO_TESTS%" == "" goto no_tests
 	cd %TEST_DIR%
 	set QT_QPA_PLATFORM=minimal
 	for /r %%f in (tst_*.exe) do (
-		start /w call %%f ^> %CD%/test.log || exit /B 1
+		start /w call %%f ^> %CD%/test.log ^|^| echo FAIL ^> fail ^& exit || exit /B 1
 		type test.log
+		if exist fail exit /B 1
 	)
 	endlocal
 	cd \projects\%CurrDirName%\build-%PLATFORM%
