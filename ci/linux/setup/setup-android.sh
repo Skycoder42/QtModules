a15b1350341556bdf96dc151be45a2e2bbcb8027
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-09-04 13:44:22 +0200

    update android sdk

--- ci/linux/setup/setup-android.sh
+++ ci/linux/setup/setup-android.sh
@@ -7,12 +7,12 @@ scriptdir=$(dirname $0)
 apt-get -qq install --no-install-recommends openjdk-8-jdk unzip
 
 # android skd/ndk
-curl -Lo /tmp/android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
+curl -Lo /tmp/android-sdk.zip https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
 mkdir $HOME/android
 unzip -qq /tmp/android-sdk.zip -d $HOME/android/sdk/
 rm -f /tmp/android-sdk.zip
 echo y | $HOME/android/sdk/tools/bin/sdkmanager --update > /dev/null
-for package in "platform-tools" "platforms;android-27" "build-tools;27.0.3" "ndk-bundle"; do
+for package in "platform-tools" "platforms;android-28" "build-tools;28.0.2" "ndk-bundle"; do
 	echo install android $package
 	echo y | $HOME/android/sdk/tools/bin/sdkmanager "$package" > /dev/null
 done
