650eb5f4d64a49551b329d71a24b7fd6f5d4bdf1
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-09-12 14:31:20 +0200

    relpath

--- ci/linux/build-flatpak.sh
+++ ci/linux/build-flatpak.sh
@@ -3,7 +3,5 @@ set -e
 
 scriptdir=$(dirname $0)
 
-manifest=$(realpath $FLATPAK_MANIFEST)
-
 cd flatpak-build
-flatpak-builder build "$FLATPAK_MANIFEST" --force-clean --repo=repo
+flatpak-builder build "../$FLATPAK_MANIFEST" --force-clean --repo=repo
