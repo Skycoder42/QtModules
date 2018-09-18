a79be2928aad1545632467950a5fedb8a736c654
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-09-18 15:45:35 +0200

    .

--- ci/linux/build-flatpak.sh
+++ ci/linux/build-flatpak.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
+export XZ_OPT=-9
+
 scriptdir=$(dirname $0)
 
 cd flatpak-build
-flatpak-builder build "../$FLATPAK_MANIFEST" --force-clean --repo=repo
+flatpak-builder build "../$FLATPAK_MANIFEST" --force-clean
+tar cJf install/flatpak_build.tar.xz build
