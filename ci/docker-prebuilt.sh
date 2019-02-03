1a4af15c3b9e70084dc4b91e6bf100b00748a419
Skycoder42 <Skycoder42@users.noreply.github.com>
2019-02-03 13:41:11 +0100

    enable prebuild script to support wasm
    with effective reuse

--- ci/docker-prebuilt.sh
+++ ci/docker-prebuilt.sh
@@ -20,12 +20,18 @@ PLATFORMS=${@:-gcc_64 android_arm64_v8a android_armv7 android_x86 emscripten}
 case "$IMAGE_TAG" in
 	full)
 		export EXTRA_MODULES="$EXTRA_MODULES .skycoder42"
+		EMSCRIPTEN_IMAGE_BASE="skycoder42/qt-build:${QT_VER}-emscripten-datasync"
+		export EMSCRIPTEN_EXTRA_MODS="$EMSCRIPTEN_EXTRA_MODS qtrestclient qtmvvm qtapng"
 		;;
 	datasync)
 		export EXTRA_MODULES="$EXTRA_MODULES .skycoder42.datasync"
+		EMSCRIPTEN_IMAGE_BASE="skycoder42/qt-build:${QT_VER}-emscripten-common"
+		export EMSCRIPTEN_EXTRA_MODS="$EMSCRIPTEN_EXTRA_MODS datasync"
 		;;
 	common)
 		export EXTRA_MODULES="$EXTRA_MODULES .skycoder42.jsonserializer .skycoder42.service"
+		EMSCRIPTEN_IMAGE_BASE="skycoder42/qt-build:${QT_VER}-emscripten-base"
+		export EMSCRIPTEN_EXTRA_MODS="$EMSCRIPTEN_EXTRA_MODS qtjsonserializer qtservice"
 		;;
 	base)
 		# no extra exports
@@ -41,9 +47,17 @@ esac
 
 for platform in $PLATFORMS; do
 	echo building $IMAGE_TAG for $platform
+	
 	export PLATFORM=$platform
+	if [ "$platform" == "emscripten" ]; then
+		export DOCKER_IMAGE_BASE=$EMSCRIPTEN_IMAGE_BASE
+	else
+		export DOCKER_IMAGE_BASE=
+	fi
+	
 	paplay /usr/share/sounds/Oxygen-Sys-App-Message.ogg || true
 	$scriptdir/linux/setup.sh
+	
 	paplay /usr/share/sounds/Oxygen-Sys-App-Message.ogg || true
 	sudo docker push "skycoder42/qt-build:${QT_VER}-${PLATFORM}-${IMAGE_TAG}"
 done
--- ci/linux/setup/setup-wasm.sh
+++ ci/linux/setup/setup-wasm.sh
@@ -4,54 +4,76 @@ set -ex
 scriptdir=$(dirname $0)
 
 export MAKEFLAGS="-j$(nproc)"
+export QDEP_CACHE_DIR="/tmp/qdep-cache"
 
-# install prequisites
-apt-get -qq update
-apt-get -qq install software-properties-common python nodejs cmake default-jre git make ca-certificates curl python3 python3-pip doxygen doxyqml $EXTRA_PKG
+if [ ! -f "/opt/qt/$QT_VER/$PLATFORM/bin/qmake" ]; then 
+	# install prequisites
+	apt-get -qq update
+	apt-get -qq install software-properties-common python nodejs cmake default-jre git make ca-certificates curl python3 python3-pip doxygen doxyqml $EXTRA_PKG
 
-# install qdep
-pip3 install qdep
+	# install qdep
+	pip3 install qdep
 
-# install emsdk
-git clone https://github.com/juj/emsdk.git /opt/emscripten-sdk
-pushd /opt/emscripten-sdk
-./emsdk install latest
-./emsdk activate latest
-source ./emsdk_env.sh
-popd
-
-# build Qt for wasm
-QT_BRANCH=v$QT_VER
-PREFIX=/opt/qt/$QT_VER/$PLATFORM
-QT_MODS="qtbase,qtwebsockets,qtdeclarative,qtremoteobjects,qtimageformats,qtsvg,qtquickcontrols2,qtgraphicaleffects,qtscxml,qtnetworkauth,qttranslations,qtxmlpatterns,qttools"
-
-tdir=$(mktemp -d)
-pushd $tdir
-git clone https://code.qt.io/qt/qt5.git ./src --branch $QT_BRANCH
-pushd src
-./init-repository --module-subset="$QT_MODS"
-
-# WASM FIX
-if [ "$QT_VER" == "5.12.1" ]; then
-	pushd qtbase
-	git config user.email "Skycoder42@users.noreply.github.com"
-	git config user.name "Skycoder42"
-	git fetch https://codereview.qt-project.org/qt/qtbase refs/changes/33/250433/2 && git cherry-pick FETCH_HEAD
+	# install emsdk
+	git clone https://github.com/juj/emsdk.git /opt/emscripten-sdk
+	pushd /opt/emscripten-sdk
+	./emsdk install latest
+	./emsdk activate latest
+	source ./emsdk_env.sh
 	popd
+
+	# build Qt for wasm
+	QT_BRANCH=v$QT_VER
+	PREFIX=/opt/qt/$QT_VER/$PLATFORM
+	QT_MODS="qtbase,qtwebsockets,qtdeclarative,qtremoteobjects,qtimageformats,qtsvg,qtquickcontrols2,qtgraphicaleffects,qtscxml,qtnetworkauth,qttranslations,qtxmlpatterns,qttools"
+
+	tdir=$(mktemp -d)
+	pushd $tdir
+	git clone https://code.qt.io/qt/qt5.git ./src --branch $QT_BRANCH
+	pushd src
+	./init-repository --module-subset="$QT_MODS"
+
+	# WASM FIX
+	if [ "$QT_VER" == "5.12.1" ]; then
+		pushd qtbase
+		git config user.email "Skycoder42@users.noreply.github.com"
+		git config user.name "Skycoder42"
+		git fetch https://codereview.qt-project.org/qt/qtbase refs/changes/33/250433/2 && git cherry-pick FETCH_HEAD
+		popd
+	fi
+
+	popd
+	mkdir build
+	pushd build
+	../src/configure -xplatform wasm-emscripten -opensource -confirm-license -make libs -prefix "$PREFIX" || (cat config.log && false)
+	make > /dev/null
+	make install
+	cp config.summary $PREFIX/config.summary
+	popd
+	popd
+
+	# prepare qdep
+	qdep prfgen --qmake "/opt/qt/$QT_VER/$PLATFORM/bin/qmake"
+else
+	source /opt/emscripten-sdk/emsdk_env.sh
 fi
 
-popd
-mkdir build
-pushd build
-../src/configure -xplatform wasm-emscripten -opensource -confirm-license -make libs -prefix "$PREFIX" || (cat config.log && false)
-make > /dev/null
-make install
-cp config.summary $PREFIX/config.summary
-popd
-popd
-
-# prepare qdep
-qdep prfgen --qmake "/opt/qt/$QT_VER/$PLATFORM/bin/qmake"
+if [ -n "$EMSCRIPTEN_EXTRA_MODS" ]; then
+	tdir=$(mktemp -d)
+	pushd $tdir
+	for extra_mod in $EMSCRIPTEN_EXTRA_MODS; do
+		git clone https://github.com/Skycoder42/${extra_mod}.git
+		pushd "$extra_mod"
+		git checkout $(git describe --tags)
+		mkdir build
+		cd build
+		/opt/qt/$QT_VER/$PLATFORM/bin/qmake "CONFIG+=no_auto_lupdate" "QT_PLATFORM=$PLATFORM" ../
+		make
+		make install
+		popd
+	done
+	popd
+fi
 
 # cleanup
 rm -rf /opt/emscripten-sdk/.git
