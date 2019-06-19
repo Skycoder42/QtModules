a8ebb9ee75163850ab82424c903eeda27828c972
Skycoder42 <Skycoder42@users.noreply.github.com>
2019-06-19 13:50:00 +0200

    no wasm lts yet

--- ci/docker-prebuilt.sh
+++ ci/docker-prebuilt.sh
@@ -40,7 +40,7 @@ case "$IMAGE_TAG" in
 		export BASE_IMAGE=true
 		;;
 	lts)
-		PLATFORMS=${@:-gcc_64 android_arm64_v8a android_armv7 android_x86 emscripten}
+		PLATFORMS=${@:-gcc_64 android_arm64_v8a android_armv7 android_x86}
 		export BASE_IMAGE=true
 		export IS_LTS=true
 		;;
