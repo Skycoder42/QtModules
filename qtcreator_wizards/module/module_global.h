1254b79eb05b622d620e348a2d827891d85e8808
Skycoder42 <Skycoder42@users.noreply.github.com>
2019-12-10 12:25:29 +0100

    .

--- qtcreator_wizards/module/module_global.h
+++ qtcreator_wizards/module/module_global.h
@@ -4,10 +4,14 @@
 
 #include <QtCore/qglobal.h>
 
-#if defined(QT_BUILD_%{ModuleDefine}_LIB)
-#	define Q_%{ModuleDefine}_EXPORT Q_DECL_EXPORT
+#ifndef QT_STATIC
+#  if defined(QT_BUILD_%{ModuleDefine}_LIB)
+#    define Q_%{ModuleDefine}_EXPORT Q_DECL_EXPORT
+#  else
+#    define Q_%{ModuleDefine}_EXPORT Q_DECL_IMPORT
+#  endif
 #else
-#	define Q_%{ModuleDefine}_EXPORT Q_DECL_IMPORT
+#  define Q_%{ModuleDefine}_EXPORT
 #endif
 
 #endif // %{GLOBGUARD}\
