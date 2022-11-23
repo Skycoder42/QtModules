df97662fdf42c365529870f9068b523649a59f1e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-23 21:43:15 +0000

    Adding tarfile member sanitization to extractall()

--- deploy/debbuild.py
+++ deploy/debbuild.py
@@ -114,7 +114,26 @@ def get_source(sdir, mod_url, asOrig=False):
 		with open(pname, "wb") as file:
 			file.write(sources.content)
 		with tarfile.open(pname) as archive:
-				archive.extractall(sdir)
+	def is_within_directory(directory, target):
+		
+		abs_directory = os.path.abspath(directory)
+		abs_target = os.path.abspath(target)
+	
+		prefix = os.path.commonprefix([abs_directory, abs_target])
+		
+		return prefix == abs_directory
+	
+	def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+	
+		for member in tar.getmembers():
+			member_path = os.path.join(path, member.name)
+			if not is_within_directory(path, member_path):
+				raise Exception("Attempted Path Traversal in Tar File")
+	
+		tar.extractall(path, members, numeric_owner=numeric_owner) 
+		
+	
+	safe_extract(archive, sdir)
 	else:
 		with tarfile.open(fileobj=io.BytesIO(sources.content)) as archive:
 			archive.extractall(sdir)
